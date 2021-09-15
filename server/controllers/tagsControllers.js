132464afe3566c829a0711bffe70ccb2441f0ebd
addakridler <addakridler@gmail.com>
2021-09-14 20:06:34 -0400

    updated package json limiting browsers installed babel runtime and plugin createTag middleware working but test is not
    Co-authored-by: shenkenny <shenkenny@users.noreply.github.com>

--- server/controllers/tagsControllers.js
+++ server/controllers/tagsControllers.js
@@ -5,10 +5,13 @@ const tagsController = {};
 // post to create tag
 tagsController.createTag = async (req, res, next) => {
   try {
+    console.log('in tags controller create Tag');
     const { techstackId, projectId } = req.body;
+    console.log('techid and projectid in tagControler: ', techstackId, projectId);
     const params = [ techstackId, projectId ];
     const createTagQuery = `INSERT INTO tags (techstackId, projectId) VALUES ($1,$2)`;
     const createdTag = await pool.query(createTagQuery, params);
+    console.log('createdTag in tagController: ', createdTag)
     res.locals.createdTag = createdTag;
     return next ();
   } catch (err) {
@@ -20,6 +23,7 @@ tagsController.createTag = async (req, res, next) => {
 // get to get tags
 tagsController.getTags = async (req, res, next) => {
   try {
+    console.log('in tags controller get Tag');
     const getTagsQuery = `SELECT * FROM tags`;
     const tags = await pool.query(getTagsQuery);
     res.locals.tags = tags;
